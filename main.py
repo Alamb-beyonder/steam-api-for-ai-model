24430d0c00b3649a541d43addeecbf98b7bfdc19
Alamb-beyonder <andy.lam93340@gmail.com>
2026-06-26 19:04:20 +1000

    Set up steam API key handling

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from dotenv import load_dotenv
 import requests
+import os
 
-response = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2/")
+load_dotenv()
+api_key = os.getenv("STEAM_API_KEY")
+url = f"https://api.steampowered.com/IStoreService/GetAppList/v1/?key={api_key}"
+
+response = requests.get(url)
+
 print(response.status_code)
 data = response.json()
+print(data["response"]["apps"])
 
